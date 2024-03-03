9465c7427be3064b544435b2c23fba11b94a7500
Your Name <you@example>
2024-03-03 22:03:41 +0800

    20240303

--- in_browser.js
+++ in_browser.js
@@ -1,32 +1,53 @@
 // 全局变量定义
-let your_url_for_next_entry;  // 用于获取下一个要发送Prompt的URL
-let your_submit_url;  // 提交ChatGPT返回结果的URL
+let your_url_for_next_entry = "https://www.ingted.com/gpt";  // 用于获取下一个要发送Prompt的URL
+let your_submit_url = "https://www.ingted.com/gpt";  // 提交ChatGPT返回结果的URL
 
 // 一些用于从DOM中选择需要元素的常量
-const regenerate_selector = 'button.btn:nth-child(1) > div:nth-child(1)'
+const regenerate_selector = 'div.font-semibold.select-none'
+//'button.btn:nth-child(1) > div:nth-child(1)'
 const continue_selector = '.justify-end > div:nth-child(2) > button:nth-child(1) > div:nth-child(1)'
 const content_classname = 'markdown'
 const submit_classname = 'md:bottom-3'
 
 var waiting_count = 0  // 设置一个等待计数器
 
+
+var cnt = 0
+
+function countIt(){
+    var divElements = document.querySelectorAll('div.font-semibold.select-none');
+    var filteredDivElements = Array.from(divElements).filter((element) => {
+        return element.textContent.includes('ChatGPT');
+    });
+    return filteredDivElements.length
+}
+
+
+function ifAdded(){
+    var newCnt = countIt()
+    var ifIncreased = newCnt > cnt
+    cnt = newCnt
+    if (ifIncreased){
+        console.log('responded...')
+    } else {
+        console.log('waiting...')
+    }
+    return ifIncreased
+}
+
 // 主函数
-function main(url_for_next_entry) {
-    // 从指定的URL获取下一个条目
-    fetch(url_for_next_entry).then(data=>{
-        return data.json()
-    }).then(res=>{
-        // 键入Prompt并发送
-        typePrompt(res.prompt); // **********注意**********请保证您的url返回的结果中'prompt'键的值为您希望接下来发送的prompt, 或者修改此处代码**********
+function main(ask) {
+
+        typePrompt(ask); // **********注意**********请保证您的url返回的结果中'prompt'键的值为您希望接下来发送的prompt, 或者修改此处代码**********
         sendPrompt(submit_classname);
         // 检查响应是否完成并进行处理
-        setTimeout(() => checkResponseAndProcess(regenerate_selector, content_classname), 30000);
+        setTimeout(() => checkResponseAndProcess(regenerate_selector, content_classname), 10000);
         waiting_count = 0
-    })
+    
 }
 
 // 初始运行
-main(your_url_for_next_entry);
+main("testIt again");
 setInterval(click_continue, 1000);
 
 // 根据元素类名获取页面上的最后一个元素
@@ -67,18 +88,19 @@ function getTextContentArray(divElement) {
 
 // 处理响应和处理
 function checkResponseAndProcess(selector, classname) {
-    if (!isResponseCompleted(selector)) {
-        setTimeout(() => checkResponseAndProcess(regenerate_selector, content_classname), 30000);
+    if (!ifAdded()) {
+        setTimeout(() => checkResponseAndProcess(regenerate_selector, content_classname), 10000);
         waiting_count += 1
     } else {
         let reply = getLastElementByClassName(classname);
         let result = {
-            reply: getTextContentArray(reply)
+            str: getTextContentArray(reply)
         };
-        console.log(JSON.stringify(result));
+        console.log("toServer ",JSON.stringify(result));
+        setTimeout(() => checkResponseAndProcess(regenerate_selector, content_classname), 10000);
         sendJsonData(your_submit_url, result).then(res=>{
-            console.log('sendJsonDataRes: ', res)
-            setTimeout(() => main(your_url_for_next_entry), 216000 - waiting_count*30000);
+            console.log('toGPT: ', res)
+            setTimeout(() => main(res.ask), 216000 - waiting_count*30000);
         }).catch(err=>{
             console.error(err)
         })
@@ -102,8 +124,10 @@ async function sendJsonData(submit_url, jsonData) {
     console.log(submit_url)
     let response = await fetch(submit_url, {
         method: 'POST',
+        //mode: "no-cors",
         headers: {
             'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin':'*'
         },
         body: JSON.stringify(jsonData),
     });
